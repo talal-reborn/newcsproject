9d3fc32c06e29dc51f2209f27795b0fead6dd09e
Talal Abdullah <talalsupahero@gmail.com>
2026-07-09 23:19:34 +0300

    add and remove cart funcs

--- backend.py
+++ backend.py
@@ -1,7 +1,6 @@
 import sqlite3
-import tkinter as tk
 
-#connecting db ---------------------------------------------
+# connecting db ---------------------------------------------
 
 con = sqlite3.connect('carnival_pos.db')
 cur = con.cursor()
@@ -19,57 +18,55 @@ def setup_database():
 
     con.commit()
 
-setup_database() #main query call for database creation. runs on every startup regardless.
+setup_database() # Runs every startup. Creates database table if it doesn't exist.
 
-#structures ----------------------------------------------
 
-cart = {} #a dictionary for the cart, storing item_id as key, and the quantity variable as value in the pair
+# structures ----------------------------------------------
 
-#major functions ---------------------------------------------
+cart = {} # Dictionary storing item_id as key and quantity as value.
 
-def add_item(name, price, description, imagepath): #item adding into the main screen
-    cur.execute('''INSERT INTO items (
-    item_name,
-    price,
-    description,
-    image_path) 
-    VALUES(?,?,?,?)''', (name, price, description, imagepath))
-    
+
+# major functions ---------------------------------------------
+
+def add_item(name, price, description, imagepath):
+    #adds new item in db
+
+    cur.execute('''
+    INSERT INTO items (
+        item_name,
+        price,
+        description,
+        image_path)
+    VALUES(?,?,?,?)
+    ''', (name, price, description, imagepath))
+
     con.commit()
 
+def get_items():
+    #returns all items in db to main menu
 
-def add_to_order(item_id): #adds item to cart on basis of quantity, into cart
+    cur.execute("SELECT * FROM items")
+    items = cur.fetchall()
+
+    return items
+
+
+def add_to_order(item_id):
+    #adds new item into the cart
+
     if item_id in cart:
         cart[item_id] += 1
     else:
         cart[item_id] = 1
-    
+
     print(cart)
 
-def get_items(): #get all items
-    cur.execute('SELECT * FROM items')
-    items = cur.fetchall()
-    return items
+def remove_from_order(item_id):
+    #removes item currently in cart
+    if item_id in cart:
+        if cart[item_id] == 1:
+            cart.pop(item_id)
+        else:
+            cart[item_id] -= 1
 
-
-#tkinter GUI ---------------------------------------------
-
-root = tk.Tk()
-root.title('CARNIVAL POS')
-
-items = get_items() #a collection of all items available
-
-for item in items: #loops through each item, assigning it a button, so that on click, it adds to the cart and tracks quantity
-    item_id = item[0]
-    item_name = item[1]
-    price = item[2]
-
-    button_text = item_name + ' - ' + str(price) + ' KD'
-    item_button = tk.Button(
-        root,
-        text=button_text,
-        command=lambda id=item_id: add_to_order(id)
-    )
-    item_button.pack()
-
-root.mainloop()
+    print(cart)
