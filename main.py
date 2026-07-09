2e38268e84651333e6caf5339d14456c9bd67f8e
Talal Abdullah <talalsupahero@gmail.com>
2026-07-09 23:00:21 +0300

    generalising names of vars --> items

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import tkinter as tk
 
 #connecting db ---------------------------------------------
 
-con=sqlite3.connect('carnival_pos.db')
-cur=con.cursor()
+con = sqlite3.connect('carnival_pos.db')
+cur = con.cursor()
 
 def setup_database():
     cur.execute("""
-        CREATE TABLE IF NOT EXISTS food_items (
-            food_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            food_name TEXT NOT NULL,
+        CREATE TABLE IF NOT EXISTS items (
+            item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            item_name TEXT NOT NULL,
             price REAL NOT NULL,
             description TEXT,
             image_path TEXT
@@ -23,33 +23,33 @@ setup_database() #main query call for database creation. runs on every startup r
 
 #structures ----------------------------------------------
 
-cart = {} #a dictionary for the cart, storing food_id as key, and the quantity variable as value in the pair
+cart = {} #a dictionary for the cart, storing item_id as key, and the quantity variable as value in the pair
 
 #major functions ---------------------------------------------
 
-def add_food_item(name, price, description, imagepath): #food adding into the main screen
-    cur.execute('''INSERT INTO food_items (
-    food_name,
+def add_item(name, price, description, imagepath): #item adding into the main screen
+    cur.execute('''INSERT INTO items (
+    item_name,
     price,
     description,
     image_path) 
-    VALUES(?,?,?,?)''',(name,price,description,imagepath))
+    VALUES(?,?,?,?)''', (name, price, description, imagepath))
     
     con.commit()
 
 
-def add_to_cart(food_id): #adds item to cart on basis of quantity, into cart
-    if food_id in cart: 
-        cart[food_id]+=1
+def add_to_order(item_id): #adds item to cart on basis of quantity, into cart
+    if item_id in cart:
+        cart[item_id] += 1
     else:
-        cart[food_id]=1
+        cart[item_id] = 1
     
     print(cart)
 
-def get_food_items(): #get all items of food
-    cur.execute('SELECT * FROM food_items')
-    foods = cur.fetchall()
-    return foods
+def get_items(): #get all items
+    cur.execute('SELECT * FROM items')
+    items = cur.fetchall()
+    return items
 
 
 #tkinter GUI ---------------------------------------------
@@ -57,20 +57,19 @@ def get_food_items(): #get all items of food
 root = tk.Tk()
 root.title('CARNIVAL POS')
 
-foods = get_food_items() #a colloection of all food available
+items = get_items() #a collection of all items available
 
-for food in foods: #loops thru each food, assigning them a button, so that on click, adds to cart and tracks quantity
-    food_id = food[0]
-    food_name = food[1]
-    price = food[2]
+for item in items: #loops through each item, assigning it a button, so that on click, it adds to the cart and tracks quantity
+    item_id = item[0]
+    item_name = item[1]
+    price = item[2]
 
-    button_text = food_name + '-' + str(price) + 'KD'
-    food_button = tk.Button(
+    button_text = item_name + ' - ' + str(price) + ' KD'
+    item_button = tk.Button(
         root,
         text=button_text,
-        command=lambda id=food_id: add_to_cart(id)
+        command=lambda id=item_id: add_to_order(id)
     )
-    food_button.pack()
+    item_button.pack()
 
 root.mainloop()
-
