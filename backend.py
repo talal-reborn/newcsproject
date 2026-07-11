49fd05b19e59c0abc2bd4c597f75ee251295a057
Talal Abdullah <talalsupahero@gmail.com>
2026-07-11 18:10:10 +0300

    t- complete order function is now added

--- backend.py
+++ backend.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime
 
 # connecting db ---------------------------------------------
 
@@ -14,7 +15,27 @@ def setup_database():
             description TEXT,
             image_path TEXT
         )
-    """) #tuple index 0-4
+    """) #tuple index 0-4, master menu table
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS orders (
+            order_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            total_price REAL NOT NULL,
+            timestamp DATETIME NOT NULL
+        )
+    """) #tuple index 0-2, master orders table
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS order_items (
+            order_item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            order_id INTEGER NOT NULL,
+            item_id INTEGER NOT NULL,
+            quantity INTEGER NOT NULL,
+            unit_price REAL NOT NULL,
+            FOREIGN KEY (order_id) REFERENCES orders(order_id),
+            FOREIGN KEY (item_id) REFERENCES items(item_id)
+        )
+    """) #tuple index 0-4, used to assign items to a receipt id, helps generate receipt
 
     con.commit()
 
@@ -90,6 +111,40 @@ def calculate_total():
         total += subtotal
 
     return total
+
+def complete_order():
+    #complete the order, make a receipt id for it, insert items into one and receipts into one table.
+    if not cart:
+        return None
+
+    total = calculate_total()
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    cur.execute(
+        "INSERT INTO orders (total_price, timestamp) VALUES (?, ?)",
+        (total, timestamp)
+    )
+
+    order_id = cur.lastrowid
+
+    for item_id, quantity in cart.items():
+        item = get_item(item_id)
+        unit_price = item[2]
+
+        cur.execute("""
+            INSERT INTO order_items (
+                order_id,
+                item_id,
+                quantity,
+                unit_price
+            )
+            VALUES (?, ?, ?, ?)
+        """, (order_id, item_id, quantity, unit_price))
+
+    con.commit()
+    clear_order()
+
+    return order_id
 
 def clear_order():
     #clear order from cart/complete for fresh new
